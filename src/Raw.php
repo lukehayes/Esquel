ce54742e56fb3e80ea73e5479d6eddec1d5f6608
lukehayes <lukepurplehayes@gmail.com>
2025-06-03 09:56:10 +0100

    refactor: corrected indentation

--- src/Raw.php
+++ src/Raw.php
@@ -8,28 +8,28 @@ class Raw
 
 	public function __construct() {}
 
-	/**
-	 * Generate the select from section of the SQL.
-	 *
-	 * @param array $table_columns    All of the table columns to be retrieved.
-	 *
-	 * @return string.
-	 */
-	public function select(...$table_columns) : Raw
-	{
-		$this->query .= "select";
-		$table_column_count = count($table_columns);
-
-		for($i = 0; $i <= $table_column_count - 1; $i++)
+		/**
+		 * Generate the select from section of the SQL.
+		 *
+		 * @param array $table_columns    All of the table columns to be retrieved.
+		 *
+		 * @return string.
+		 */
+		public function select(...$table_columns) : Raw
 		{
-			$this->query .= " " . $table_columns[$i] . ",";
+			$this->query .= "select";
+			$table_column_count = count($table_columns);
+
+			for($i = 0; $i <= $table_column_count - 1; $i++)
+			{
+				$this->query .= " " . $table_columns[$i] . ",";
+			}
+
+			// TODO Fix this later.
+			$this->query = rtrim($this->query, ',');
+
+			return $this;
 		}
-
-		// TODO Fix this later.
-		$this->query = rtrim($this->query, ',');
-
-		return $this;
-	}
 
 	/**
 	 * Add table for the selected columns.
